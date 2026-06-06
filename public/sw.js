d7dac75774f36a7ea5e49300c5d23bd7bd4da2e2
vicharak119 <57115507+vicharak119@users.noreply.github.com>
2026-06-06 08:45:18 +0530

    Add files via upload
    V6 Front end

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
-var CACHE="jw-v5";
-self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(["/","/manifest.json"]);}));});
+var CACHE="jw-v6";
+self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(["/","/manifest.json"]);}));});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));});
 self.addEventListener("fetch",function(e){if(e.request.url.indexOf("/api/")>-1)return;e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
